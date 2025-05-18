fb11a725eab5494fd2158022b0a604ffd9863eb0
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-18 18:44:56 +0530

    Cutting Edge

---
+++ src/apis/prompt_generation/handleStream.js
@@ -0,0 +1,82 @@
+import useMessageStore from "../../store/useMessagesStore";
+
+const handleStream = async (
+  id,
+  prompt,
+  onProgress,
+  onStart,
+  onEnd,
+  onError
+) => {
+  const setProcess = useMessageStore.getState().setProcess;
+  try {
+    const controller = new AbortController();
+    setProcess({
+      type: "GENERATION",
+      process_name: "Getting Started...",
+      id: id,
+    });
+    onStart?.({ id });
+    const response = await fetch(
+      "https://pa-dev-api.thesynapses.com/generate",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          type: "global",
+          prompt: prompt,
+          chat_uid: "f7bce2c1-37bd-4f79-8fca-86fc9a851b6e",
+          file_url: [],
+          org_id: "synapses",
+          uid: "un2xqHu71cd6WWycTr1P6UE4PiJ2",
+          regenerate: false,
+          style: "Standard",
+          model_id: "gemini-2.0-flash-001",
+          recaching: false,
+          google_search: false,
+          cache_id: null,
+          file_data: "",
+          prompt_id: id,
+          new_prompt: "",
+          by: "un2xqHu71cd6WWycTr1P6UE4PiJ2",
+        }),
+        signal: controller.signal,
+      }
+    );
+
+    if (!response.ok || !response.body) {
+      setProcess(null);
+      throw new Error("Stream error");
+    }
+
+    setProcess(
+      {
+        type: "GENERATION",
+        process_name: "Generating...",
+        id: id,
+      },
+      controller
+    );
+
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    for (let done = false; !done; ) {
+      const { value, done: readerDone } = await reader.read();
+      done = readerDone;
+      if (value) {
+        const chunk = decoder.decode(value, { stream: true });
+        onProgress?.({ id, chunk });
+      }
+    }
+
+    setProcess(null);
+    onEnd?.();
+  } catch (err) {
+    setProcess(null);
+    onError?.();
+  }
+};
+
+export default handleStream;
---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export const scrollToMessage = (id, duration = 100) => {
+  setTimeout(() => {
+    const messageEle = document.getElementById(id);
+    const container = document.getElementById("messages-container");
+    if (messageEle && container) {
+      const topPos = messageEle.offsetTop;
+      container.scrollTo({
+        top: topPos,
+        behavior: "smooth",
+      });
+    }
+  }, duration);
+};
