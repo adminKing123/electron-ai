8e53140c22728539c57620d775d148892cb9bc3b
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-23 19:03:12 +0530

    MdImage

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -1,10 +1,25 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+const MdImage = ({ src, alt }) => {
+  return (
+    <div className="max-w-[400px] rounded-lg overflow-hidden">
+      <img src={src} alt={alt} className="h-full" loading="lazy" />
+    </div>
+  );
+};
+
 const MarkdownRenderer = ({ content }) => {
   return (
     <div className="markdown-body">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          img: ({ node, ...props }) => <MdImage {...props} />,
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 };
