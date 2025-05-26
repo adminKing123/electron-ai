0f1e9305fbb8e11bfff4d53e12973fd0efa092ee
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-26 13:35:54 +0530

    ENV ADDED

--- src/page_layouts/MainLayout.js
+++ src/page_layouts/MainLayout.js
@@ -1,19 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
+import useUserStore from "../store/useUserStore";
+import ROUTES from "../router/routes";
 
 const MainLayout = () => {
-  return (
-    <div className="bg-[#212121] w-screen h-[100dvh] flex">
-      <Sidebar />
-      <div className="w-full min-w-0 h-full flex-grow relative">
-        <Header />
-        <div className="h-[calc(100%-64px)]">
-          <Outlet />
+  const { user } = useUserStore();
+  if (user)
+    return (
+      <div className="bg-[#212121] w-screen h-[100dvh] flex">
+        <Sidebar />
+        <div className="w-full min-w-0 h-full flex-grow relative">
+          <Header />
+          <div className="h-[calc(100%-64px)]">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  return <Navigate to={ROUTES.AUTH} />;
 };
 
 export default MainLayout;
--- src/pages/GenerationPage.js
+++ src/pages/GenerationPage.js
@@ -1,15 +1,9 @@
 import Prompt from "../components/Prompt";
 import Messages from "../components/Messages";
 import { v4 as uuidv4 } from "uuid";
-import useUserStore from "../store/useUserStore";
-import { Navigate } from "react-router-dom";
-import ROUTES from "../router/routes";
 
 function GenerationPage() {
-  const { user } = useUserStore();
   const chat_id = `temp-${uuidv4()}`;
-
-  if (!user) return <Navigate to={ROUTES.AUTH} />;
   return (
     <div className="flex flex-col h-full w-full">
       <Messages chat_id={chat_id} />
