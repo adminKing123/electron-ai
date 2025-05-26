c87c7475f736e3bf9fe1911a63d56716e9dc6fdd
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-26 13:47:31 +0530

    ADDED ENV

--- src/config.js
+++ src/config.js
@@ -1,23 +1,23 @@
 const CONFIG = {
-  API_BASE_URL: "https://pa-dev-api.thesynapses.com",
+  API_BASE_URL: process.env.REACT_APP_API_URL,
   GET_GENERATE_URL: function (data) {
     let endpoint = "/generate";
     if (data.generate_image) endpoint = "/generate-image";
     return `${this.API_BASE_URL}${endpoint}`;
   },
   DEFAULT_USER: {
-    uid: "un2xqHu71cd6WWycTr1P6UE4PiJ2",
-    org_id: "synapses",
+    uid: process.env.REACT_APP_DEFAULT_USER_UID,
+    org_id: process.env.REACT_APP_DEFAULT_USER_ORG_ID,
   },
   CALL_GET_AI_MODELS_API: false,
   FIREBASE_CONFIG: {
-    apiKey: "AIzaSyAoe4VpGoJA0RZHWvTji7__eSnVCTZ29ek",
-    authDomain: "electron-ai-dc720.firebaseapp.com",
-    projectId: "electron-ai-dc720",
-    storageBucket: "electron-ai-dc720.firebasestorage.app",
-    messagingSenderId: "508739640467",
-    appId: "1:508739640467:web:f0127c23b10da7e8fc8166",
-    measurementId: "G-3H1H3BRE2J",
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID,
   },
 };
 
