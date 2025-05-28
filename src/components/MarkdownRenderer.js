9f1ab836df918fa19dce99f8df0489048b1bab31
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-28 18:57:47 +0530

    List Bolllet

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -16,6 +16,15 @@ const MarkdownRenderer = ({ content }) => {
         remarkPlugins={[remarkGfm]}
         components={{
           img: ({ node, ...props }) => <MdImage {...props} />,
+          ul: ({ children }) => (
+            <ul className="revert-list-style">{children}</ul>
+          ),
+          ol: ({ children }) => (
+            <ol className="revert-list-style">{children}</ol>
+          ),
+          li: ({ children }) => (
+            <li className="revert-list-style">{children}</li>
+          ),
         }}
       >
         {content}
