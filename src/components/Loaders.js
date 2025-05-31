286cd685be54f6ec8a3e5995b0f32da1a92a5fe8
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-31 14:59:15 +0530

    loader color fixed

--- src/components/Loaders.js
+++ src/components/Loaders.js
@@ -2,7 +2,7 @@ export const GettingStartedLoader = () => {
   return (
     <div className="flex items-center gap-2">
       <div className="flex items-center justify-center">
-        <div className="w-3 h-3 border-2 border-t-transparent border-black dark:border-white rounded-full animate-spin"></div>
+        <div className="w-3 h-3 border-2 border-t-transparent border-black dark:border-t-transparent dark:border-white rounded-full animate-spin"></div>
       </div>
       <div className="text-black dark:text-white text-xs">Getting Started</div>
     </div>
