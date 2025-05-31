4009c38acfb7dab4fdb0cb18c5d672b91ee60ef1
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-05-31 15:16:40 +0530

    text color changes

--- src/components/ChatGreetings.js
+++ src/components/ChatGreetings.js
@@ -1,6 +1,6 @@
 function ChatGreetings({ chat }) {
   return chat.is_new ? (
-    <h2 className="w-full text-center text-[28px] absolute bottom-[140px]">
+    <h2 className="text-black dark:text-white w-full text-center text-[28px] absolute bottom-[140px]">
       What can I help with?
     </h2>
   ) : null;
