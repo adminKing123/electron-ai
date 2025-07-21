ddcdcbc88ab113673b234a74d24af58dcc14d5c7
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-07-21 18:05:08 +0530

    CONFIG PROMPT_ACTION_TYPES

--- src/components/MessageRelatedActionsInPrompt/index.js
+++ src/components/MessageRelatedActionsInPrompt/index.js
@@ -1,10 +1,12 @@
+import CONFIG from "../../config";
 import usePromptStore from "../../store/usePromptStores";
 import MessageBeingEdited from "./MessageBeingEdited";
 
 function MessageRelatedActionsInPrompt() {
   const action = usePromptStore((state) => state.action);
 
-  if (action?.type === "EDIT") return <MessageBeingEdited action={action} />;
+  if (action?.type === CONFIG.PROMPT_ACTION_TYPES.EDIT)
+    return <MessageBeingEdited action={action} />;
   return null;
 }
 
