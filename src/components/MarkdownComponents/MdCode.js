1b7154b4bcedb4e12f47e291a562a665256d4749
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-07-25 14:34:32 +0530

    colors and syntax highlighter fixed

--- src/components/MarkdownComponents/MdCode.js
+++ src/components/MarkdownComponents/MdCode.js
@@ -1,4 +1,4 @@
-// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import MdCodeCopyButton from "./MdCodeCopyButton";
 
 const MdCode = ({ lang = "", children, ...props }) => {
@@ -13,22 +13,22 @@ const MdCode = ({ lang = "", children, ...props }) => {
 
   return (
     <div className="my-4 bg-[#F9F9F9] dark:bg-[#171717] border border-[#E4E4E4] dark:border-[#454545] rounded-2xl overflow-hidden">
-      <div className="px-[18px] py-2 bg-[#f1f1f1] dark:bg-[#2F2F2F] rounded-t-2xl flex justify-between items-center">
+      <div className="px-[18px] py-2 bg-[#f1f1f1] dark:bg-[#1d1d1d] rounded-t-2xl flex justify-between items-center">
         <div className="font-bold text-black dark:text-white">{lang}</div>
         <MdCodeCopyButton handleCopy={handleCopy} />
       </div>
       <div className="p-4">
-        {/* <SyntaxHighlighter
+        <SyntaxHighlighter
           language={lang}
           useInlineStyles={false}
           style={{}}
           customStyle={{ margin: 0 }}
         >
           {String(children).replace(/\n$/, "")}
-        </SyntaxHighlighter> */}
-        <pre className="!m-0">
+        </SyntaxHighlighter>
+        {/* <pre className="!m-0">
           <code className="text-nowrap">{children}</code>
-        </pre>
+        </pre> */}
       </div>
     </div>
   );
