9c9cbd797ffc08a7560ee31461b6db2785091f94
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-07-29 10:38:39 +0530

    Dynamic Sources UI

--- src/components/MainSideLayout/SourcesSideLayout.js
+++ src/components/MainSideLayout/SourcesSideLayout.js
@@ -39,22 +39,24 @@ function SourcesSideLayout() {
   };
 
   return (
-    <div className="w-[380px] flex-shrink-0 border-l border-[#f2f2f2] dark:border-[#303030]">
-      <div className="px-4 header-shadow mb-[1px] flex items-center justify-between h-14">
-        <h2 className="text-lg font-semibold text-[#000000] dark:text-[#ffffff]">
-          Sources
-        </h2>
-        <button
-          onClick={toggleSideLayout}
-          className="w-10 h-10 px-2 hover:bg-[#EAEAEA] dark:hover:bg-[#3A3A3A] rounded-lg"
-        >
-          <IoClose className="text-[#8F8F8F] dark:text-white w-[24px] h-[24px]" />
-        </button>
-      </div>
-      <div className="flex flex-col gap-2 p-2 h-[calc(100%-57px)] overflow-y-auto">
-        {data?.sources.map((source, index) => (
-          <Source key={index} data={source} />
-        ))}
+    <div className="fixed lg:static top-0 left-0 w-screen lg:w-[380px] lg:flex-shrink-0 h-screen z-[100] lg:z-auto p-5 lg:p-0 flex justify-center lg:justify-normal items-center lg:items-stretch bg-[#2f2f2fa3] dark:bg-[#00000076] lg:bg-transparent">
+      <div className="rounded-xl lg:rounded-none min-w-[280px] lg:min-w-0 max-w-[640px] lg:max-w-none w-full h-auto lg:h-full bg-[#ffffff] dark:bg-[#212121] shadow-xl lg:shadow-none border border-[#f2f2f2] dark:border-[#303030] flex flex-col">
+        <div className="px-4 header-shadow mb-[1px] flex items-center justify-between h-14">
+          <h2 className="text-lg font-semibold text-[#000000] dark:text-[#ffffff]">
+            Sources
+          </h2>
+          <button
+            onClick={toggleSideLayout}
+            className="w-10 h-10 px-2 hover:bg-[#EAEAEA] dark:hover:bg-[#3A3A3A] rounded-lg"
+          >
+            <IoClose className="text-[#8F8F8F] dark:text-white w-[24px] h-[24px]" />
+          </button>
+        </div>
+        <div className="flex flex-col gap-2 p-2 max-h-[80vh] lg:max-h-none lg:h-[calc(100%-57px)] overflow-y-auto">
+          {data?.sources.map((source, index) => (
+            <Source key={index} data={source} />
+          ))}
+        </div>
       </div>
     </div>
   );
