1853967bf867d30a0ec2aee5f761fc76e6a09f90
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-07-31 10:58:21 +0530

    Wow

--- src/components/MainSideLayout/index.js
+++ src/components/MainSideLayout/index.js
@@ -1,11 +1,21 @@
 import useMainSideLayoutStore from "../../store/useMainSideLayoutStore";
 import SourcesSideLayout from "./SourcesSideLayout";
+import { AnimatePresence } from "framer-motion";
 
 const MainSideLayout = () => {
   const data = useMainSideLayoutStore((state) => state.data);
 
-  if (data?.type === "sources") return <SourcesSideLayout />;
-  else return null;
+  const COMPONENTS = {
+    sources: SourcesSideLayout,
+  };
+
+  const Component = COMPONENTS?.[data?.type];
+
+  return (
+    <AnimatePresence>
+      {Component ? <Component data={data} /> : null}
+    </AnimatePresence>
+  );
 };
 
 export default MainSideLayout;
