1727c976bf8e48f9143df8ba729ed44009272a8b
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-08-05 17:34:04 +0530

    some more UI updates

--- src/components/MessageRelatedActionsInPrompt/MessageBeingEdited.js
+++ src/components/MessageRelatedActionsInPrompt/MessageBeingEdited.js
@@ -1,6 +1,7 @@
 import { IoClose } from "react-icons/io5";
 import usePromptStore from "../../store/usePromptStores";
 import { focusPromptTextArea, scrollToMessageTop } from "../../utils/helpers";
+import { FiEdit2 } from "react-icons/fi";
 
 const MessageBeingEdited = ({ action }) => {
   const setAction = usePromptStore((state) => state.setAction);
@@ -19,12 +20,16 @@ const MessageBeingEdited = ({ action }) => {
   return (
     <div
       onClick={handleClick}
-      className="flex items-center gap-2 font-sans px-5 py-2 mx-1 mt-1 text-black dark:text-white bg-[#f0f0f0] dark:bg-[#444444] rounded-t-[18px] rounded-b-lg cursor-pointer"
+      className="flex items-center gap-2 font-sans px-5 py-2 mx-1 mt-1 text-black dark:text-white bg-[#f0f0f0] dark:bg-[#444444] rounded-t-[18px] rounded-b-lg cursor-pointer hover:bg-[#e0e0e0] dark:hover:bg-[#3a3a3a]"
     >
+      <FiEdit2 className="text-[11px] flex-shrink-0" />
       <div className="line-clamp-2 text-[11px] flex-grow">
         {action.data.prompt}
       </div>
-      <button onClick={handleCancel} className="p-[6px] hover:bg-[#EAEAEA] dark:hover:bg-[#3A3A3A] rounded-lg">
+      <button
+        onClick={handleCancel}
+        className="p-[6px] hover:bg-[#EAEAEA] dark:hover:bg-[#3A3A3A] rounded-lg"
+      >
         <IoClose />
       </button>
     </div>
