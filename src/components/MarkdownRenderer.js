86c294cb9ee4d3d84268373efb6c8de656275fd8
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-08-07 11:54:29 +0530

    Best Optimization I have ever did

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
@@ -7,28 +8,32 @@ import MdCode from "./MarkdownComponents/MdCode";
 import MdImage from "./MarkdownComponents/MdImage";
 
 const MarkdownRenderer = ({ content }) => {
+  const components = useMemo(
+    () => ({
+      img: ({ node, ...props }) => <MdImage {...props} />,
+      code({ node, inline, className, children, ...props }) {
+        const match = /language-(\w+)/.exec(className || "");
+        const lang = match?.[1];
+
+        return !inline && match ? (
+          <MdCode lang={lang} {...props}>
+            {children}
+          </MdCode>
+        ) : (
+          <code className={className} {...props}>
+            {children}
+          </code>
+        );
+      },
+    }),
+    []
+  );
   return (
     <div className="markdown-body">
       <ReactMarkdown
         remarkPlugins={[remarkGfm, remarkMath]}
         rehypePlugins={[rehypeKatex]}
-        components={{
-          img: ({ node, ...props }) => <MdImage {...props} />,
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || "");
-            const lang = match?.[1];
-
-            return !inline && match ? (
-              <MdCode lang={lang} {...props}>
-                {children}
-              </MdCode>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          },
-        }}
+        components={components}
       >
         {content}
       </ReactMarkdown>
