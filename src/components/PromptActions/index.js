b8e7982e44ca9c22b42041a4b5623a0bfba5cd7f
adminKing123 <116816518+adminKing123@users.noreply.github.com>
2026-02-08 19:32:10 +0530

    Remove SearchOptionsPopover from PromptActions
    Removed SearchOptionsPopover component from PromptActions.

--- src/components/PromptActions/index.js
+++ src/components/PromptActions/index.js
@@ -1,7 +1,6 @@
 import FileInputTaker from "./FileInputTaker";
 import SelectAIModel from "./SelectAIModel";
 import SendButton from "./SendButton";
-import SearchOptionsPopover from "./SearchOptionsPopover";
 import MicButton from "./MicButton";
 
 const PromptActions = ({ handleSend, fileInputRef, chat }) => {
@@ -10,7 +9,6 @@ const PromptActions = ({ handleSend, fileInputRef, chat }) => {
       <div className="flex items-center gap-2">
         <SelectAIModel />
         <FileInputTaker inputRef={fileInputRef} chat={chat} />
-        <SearchOptionsPopover />
       </div>
       <div className="flex items-center gap-2">
         <MicButton />
